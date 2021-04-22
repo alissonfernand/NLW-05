06753af713b726c5e8dcf4f7b23c01440c770dda
alissonfernandes <alisson.flutter@gmail.com>
2021-04-21 23:20:50 -0300

    atualizando component welcome

--- plantmanager/src/pages/Welcome.tsx
+++ plantmanager/src/pages/Welcome.tsx
@@ -1,24 +1,41 @@
 import React from 'react';
+import { Dimensions, TouchableOpacity, View } from 'react-native';
 import { Image, Text, SafeAreaView, StyleSheet } from "react-native"
 
 import watering from '../assets/watering.png';
-import Button from '../components/Button';
 import colors from '../styles/colors';
+import { Feather } from '@expo/vector-icons';
+import fonts from '../styles/fonts';
+import { useNavigation } from '@react-navigation/core';
 
 const Welcome = () => {
+  const navigation = useNavigation();
+
+  function handleStart() {
+    navigation.navigate('UserIdentification');
+  }
+
   return(
     <SafeAreaView style={styles.container}>
+      <View style={styles.wrapper}>
         <Text style={styles.title}>
           Gerencie {'\n'}
           suas plantas {'\n'}
           de forma fácil
         </Text>
 
-        <Image style={styles.image} source={watering} />
+        <Image style={styles.image} source={watering} resizeMode="contain" />
 
-        <Text style={styles.subtitle}>Não esqueça mais de regar suas {'\n'} plantas. Nós cuidamos de lembrar você{'\n'}  sempre que precisar</Text>
+        <Text style={styles.subtitle}>Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você sempre que precisar</Text>
 
-        <Button title=">" />
+        <TouchableOpacity
+          style={styles.button}
+          activeOpacity={0.7}
+          onPress={handleStart}
+        >
+          <Feather name="chevron-right" style={styles.buttonIcon} />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   )
 }
@@ -28,24 +45,43 @@ export default Welcome;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  wrapper: {
+    flex: 1,
     justifyContent: 'space-evenly',
     alignItems: 'center',
+    paddingHorizontal: 20,
     paddingVertical: 20,
   },
   title: {
-    fontSize: 32,
+    fontFamily: fonts.heading,
+    fontSize: 28,
     fontWeight: 'bold',
     textAlign: 'center',
     color: colors.heading,
+    lineHeight: 34,
   },
   image: {
-    width: 292,
-    height: 284,
+    height: Dimensions.get('window').width * 0.7,
   },
   subtitle: {
-    fontSize: 17,
-    lineHeight: 25,
     textAlign: 'center',
+    fontSize: 18,
+    paddingHorizontal: 20,
     color: colors.heading,
+    fontFamily: fonts.text,
+  },
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 16,
+    marginBottom: 10,
+    height: 56,
+    width: 56,
+  },
+  buttonIcon: {
+    fontSize: 32,
+    color: colors.white
   }
 })
